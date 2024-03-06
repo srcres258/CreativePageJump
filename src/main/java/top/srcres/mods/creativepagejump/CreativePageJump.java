ba7f2f339628e174ba9e4b198257adfd8d5f7e49
src_resources <src.res.211@gmail.com>
2024-03-06 11:08:55 +0800

    Add client-side judgement
    Since this mod is only client-sided, add a judgement to ensure it will
    never be loaded anywhere else.
    
    Signed-off-by: src_resources <src.res.211@gmail.com>

--- src/main/java/top/srcres/mods/creativepagejump/CreativePageJump.java
+++ src/main/java/top/srcres/mods/creativepagejump/CreativePageJump.java
@@ -1,8 +1,17 @@
 package top.srcres.mods.creativepagejump;
 
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLEnvironment;
 
 @Mod(CreativePageJump.MODID)
 public class CreativePageJump {
     public static final String MODID = "creativepagejump";
+
+    public CreativePageJump(IEventBus modEventBus) {
+        // This mod is only client-sided.
+        if (!FMLEnvironment.dist.equals(Dist.CLIENT))
+            throw new RuntimeException(String.format("The mod %s is only client-sided.", MODID));
+    }
 }
